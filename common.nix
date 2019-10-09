6c304c306b30348235e546455742570c5b066000
Marijan <marijan.petricevic94@gmail.com>
2019-10-09 10:27:16 +0200

    updated configurations

--- common.nix
+++ common.nix
@@ -56,12 +56,6 @@
       ];
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should
-  system.stateVersion = "19.03";
-  
   # Automatic Upgrades
   system.autoUpgrade.enable = true;
   system.autoUpgrade.channel = https://nixos.org/channels/nixos-unstable;
--- machines/kamara/configuration.nix
+++ machines/kamara/configuration.nix
@@ -1,17 +1,9 @@
 { config, pkgs, lib, ... }:
 {
-  # NixOS wants to enable GRUB by default
   boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
- 
-  # !!! Otherwise (even if you have a Raspberry Pi 2 or 3), pick this:
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
-  # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
-  # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
-  boot.kernelParams = ["cma=64M"];
+  boot.kernelParams = ["cma=256M"];
     
   # File systems configuration for using the installer's partition layout
   fileSystems = {
