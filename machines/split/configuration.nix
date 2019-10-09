c2a30ea7c06af46d83da71476aa088743a20e787
Marijan <marijan.petricevic94@gmail.com>
2019-10-09 18:59:55 +0200

    changed configuration for split

--- machines/split/configuration.nix
+++ machines/split/configuration.nix
@@ -1,58 +1,38 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
+let
+  splitpkgs = <splitpkgs> { };
+in
+
 {
-
-  networking.hostName = "split";
-
   imports =
-    [ # Include the results of the hardware scan.
-      ../common.nix
+    [
+      ./hardware-configuration.nix
+      ./common.nix
+ #     ./qemu.nix
     ];
 
-  
+  #qemu-user = {
+  #    aarch64 = true;
+  #};
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  networking.hostName = "split"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  nix = {
-    extraOptions = ''
-      build-cores = 4
-    '';
-    maxJobs = 4;
-  };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
-    wget vim git zsh tmux zip unzip firefox pulseaudio python python3 vscode
+    firefox
+    pulseaudio
+    python
+    python3
+    vscode
+    (python3.withPackages(ps: with ps; [ numpy ]))
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable sound.
   sound.enable = true;
@@ -69,5 +49,4 @@
   # Enable the KDE Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-
 }
