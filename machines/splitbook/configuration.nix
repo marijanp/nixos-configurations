4283d2cac757c5bb21e13a1d779c4ec5caae1da5
Marijan <marijan.petricevic94@gmail.com>
2019-10-30 16:55:17 +0100

    updated splitbook configuration.nix

--- machines/splitbook/configuration.nix
+++ machines/splitbook/configuration.nix
@@ -13,9 +13,10 @@
   ];
 
   networking.hostName = "splitbook"; # Define your hostname.
+  networking.interfaces.wlp3s0.useDHCP = true;
   
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = true;
+  networking.useDHCP = false;
 }
