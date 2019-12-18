e342041d7a8bb0ce66dd09d1bfad34ed5aa49543
Marijan Petricevic <marijan.petricevic94@gmail.com>
2019-12-18 10:41:41 +0100

    added spi kernel modules

--- machines/splitberry/hardware-configuration.nix
+++ machines/splitberry/hardware-configuration.nix
@@ -1,11 +1,19 @@
 { config, pkgs, lib, ... }:
 {
+  hardware.deviceTree.enable = true;
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.raspberryPi.version = 3;
+  boot.loader.raspberryPi.uboot.enable = true;
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = ["cma=256M"];
   hardware.enableRedistributableFirmware = true;
-  boot.kernelModules = ["i2c-dev"];
+  boot.kernelModules = [ "i2c-dev" "i2c_bcm2835" "spidev" "spi_bcm2835" ];
+  boot.loader.raspberryPi.firmwareConfig = ''
+      dtparam=spi=on
+      dtoverlay=spi0-hw-cs
+  '';
  
   # File systems configuration for using the installer's partition layout
   fileSystems = {
