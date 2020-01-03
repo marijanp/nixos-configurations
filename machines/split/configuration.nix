6aab74ed9cd7cd129a8f3c9febdb200421a2ec06
Marijan Petricevic <marijan.petricevic94@gmail.com>
2020-01-03 22:20:01 +0100

    enabled wireless networking

--- machines/split/configuration.nix
+++ machines/split/configuration.nix
@@ -7,10 +7,15 @@
     ./common.nix
     ./environments/desktop.nix
     ./services/avahi.nix
+    ./environments/hydra.nix
+    ./networking/wireless.nix
  #     ./qemu.nix
   ];
 
-  networking.hostName = "split"; # Define your hostname.
+  networking.hostName = "split";
+  networking.interfaces.eno1.useDHCP = true;
+  networking.interfaces.wlp3s0u1.useDHCP = true;
+  networking.useDHCP = false;
 
   #qemu-user = {
   #    aarch64 = true;
