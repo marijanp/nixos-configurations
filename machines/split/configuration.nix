27472ebcee8815f2546b117bbdf87432d7451a4e
Marijan Petričević <marijan.petricevic94@gmail.com>
2020-09-11 14:19:53 +0200

    split: use static ip for wifi network

--- machines/split/configuration.nix
+++ machines/split/configuration.nix
@@ -13,6 +13,14 @@
 
   networking.hostName = "split";
   networking.interfaces.eno1.useDHCP = true;
-  networking.interfaces.wlp3s0u1.useDHCP = true;
+  networking.interfaces.wlp3s0u1 = {
+    useDHCP = false;
+    ipv4.addresses = [ {
+      address = "192.168.2.112";
+      prefixLength = 24;
+    } ];
+  };
+  networking.defaultGateway = "192.168.2.1";
+  networking.nameservers = ["8.8.8.8"];
   networking.useDHCP = false;
 }
