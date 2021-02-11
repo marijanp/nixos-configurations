3fa6518d1525cca06cabb4bf5689aa9c5af431de
Marijan Petričević <marijan.petricevic94@gmail.com>
2021-02-11 18:45:32 +0100

    splitberry: update config

--- machines/splitberry/configuration.nix
+++ machines/splitberry/configuration.nix
@@ -3,11 +3,8 @@
   imports = [
     ./hardware-configuration.nix
     ../../common.nix
-    ../../networking/wireless.nix
-    ../../services/avahi.nix
   ];
+
   networking.hostName = "splitberry";
-  networking.interfaces.wlan0.useDHCP = true;
-  networking.interfaces.eth0.useDHCP = true;
-  networking.useDHCP = false;
+  networking.nameservers = ["8.8.8.8"];
 }
--- machines/splitberry/hardware-configuration.nix
+++ machines/splitberry/hardware-configuration.nix
@@ -1,24 +1,9 @@
 { config, pkgs, lib, ... }:
 {
-  #boot.kernelModules = [ "i2c-dev" "i2c_bcm2835" "spidev" "spi_bcm2835" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = ["cma=256M"];
-
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
-
+  boot.kernelPackages = pkgs.linuxPackages;
   
-  boot.loader.raspberryPi.enable = true;
-  boot.loader.raspberryPi.version = 3;
-  boot.loader.raspberryPi.uboot.enable = true;
-  
-  boot.loader.raspberryPi.firmwareConfig = ''
-  '';
-  #dtparam=spi=on
-
-  hardware.enableRedistributableFirmware = true;  # for wifi on rpi
-  #hardware.deviceTree.enable = true;
-
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
@@ -26,6 +11,5 @@
     };
   };
     
-  # !!! Adding a swap file is optional, but strongly recommended!
   swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 }
