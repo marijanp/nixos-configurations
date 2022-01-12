d70ad22940724fed72b26d0a4fca311d15508af0
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-01-12 19:10:09 +0100

    machines/splitberry: update

--- machines/splitberry/configuration.nix
+++ machines/splitberry/configuration.nix
@@ -1,10 +1,26 @@
 { config, pkgs, lib, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
 {
   imports = [
     ./hardware-configuration.nix
-    ../../common.nix
+    
+    (import "${home-manager}/nixos")
+
+    ../../users/marijan/base.nix
+    ../../environments/common.nix
+    ../../options/wireless.nix
   ];
 
-  networking.hostName = "splitberry";
-  networking.nameservers = ["8.8.8.8"];
+  home-manager.users.marijan = import ../../dotfiles/common.nix;
+
+  networking = {
+    hostName = "splitberry";
+    nameservers = ["8.8.8.8"];
+    wireless = {
+      enable = true;
+      interfaces = [ "wlan0" ];
+    };
+  };
 }
--- machines/splitberry/hardware-configuration.nix
+++ machines/splitberry/hardware-configuration.nix
@@ -1,15 +1,19 @@
 { config, pkgs, lib, ... }:
-{
+
   boot.loader.grub.enable = false;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.generic-extlinux-compatible.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages;
-  
+  boot.loader.raspberryPi = {
+    enable = true;
+    version = 3;
+    uboot.enable = true;
+  };
+
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
+      options = [ "noatime" ];
     };
   };
-    
-  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 }
