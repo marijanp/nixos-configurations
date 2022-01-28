a872fea77a7b81544ae5c16906528ba3323e22dd
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-01-28 10:29:58 +0100

    splitberry: add nastavi cronjob

--- machines/splitberry/configuration.nix
+++ machines/splitberry/configuration.nix
@@ -1,6 +1,10 @@
 { config, pkgs, lib, ... }:
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  splitpkgs = import (builtins.fetchGit {
+    url = "git@github.com:marijanp/splitpkgs.git";
+    ref = "refs/heads/master";
+  }) { };
 in
 {
   imports = [
@@ -18,6 +22,15 @@ in
     vim
   ];
 #  home-manager.users.marijan = import ../../users/marijan/home.nix;
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      ''
+      */15 * * * * marijan ${splitpkgs.tp-link-sms}/bin/sms-send --url="http://192.168.1.1" --login=admin --password='3>nf>cm(@RPLWa-h)U/k"/,Z' 13880 'NASTAVI' >> /tmp/nastavi.log
+      ''
+    ];
+  };
 
   networking = {
     hostName = "splitberry";
