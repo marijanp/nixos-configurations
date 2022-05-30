b864bfa137e2058c204aaafbf41734b989048d17
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-05-30 10:52:23 +0200

    formatting

--- dotfiles/common.nix
+++ dotfiles/common.nix
@@ -12,7 +12,7 @@
       vim = "vim -X";
     };
     profileExtra = ''
-    export PS1="📅 \d ⌚️ \A\n\[\e[36m\]\u@\H\[\e[m\] [\w]\$ "
+      export PS1="📅 \d ⌚️ \A\n\[\e[36m\]\u@\H\[\e[m\] [\w]\$ "
     '';
   };
 
@@ -42,6 +42,7 @@
       :set number relativenumber
       :inoremap jk <Esc>l:w<CR>
       :vnoremap jk <Esc>l:w<CR>
+      "disable arrow keys and escape
       noremap <Up> <NOP>
       noremap <Down> <NOP>
       noremap <Left> <NOP>
@@ -53,7 +54,11 @@
       inoremap <Esc> <NOP>
       vnoremap <Esc> <NOP>
       set t_BE=                 "Fix paste bug triggered by the above inoremaps
-      set tabstop=2 expandtab
+      nnoremap <C-Z> :Lex<CR>:vertical resize 50<CR><C-C>
+      set tabstop=2             "intendation width to 2 spaces
+      set expandtab             "use spaces instead of tab
+      set shiftwidth=2          "autoindent with 2 spaces
+      set smartindent
       syntax on
       filetype plugin indent on "enable the listed plugins in this file
     '';
