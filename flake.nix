51ac8d1e111db2a817f0c50cdcde06d2bddec4d9
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-06-20 18:59:57 +0200

    flake: make splitpkgs use same package

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
     home-manager.url = github:nix-community/home-manager/release-22.05;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    splitpkgs.url = "git+ssh://git@github.com/marijanp/splitpkgs.git";
+    splitpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, home-manager, splitpkgs }: {
     nixosConfigurations = {
       split =  nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -28,9 +30,26 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.marijan = import ./dotfiles/work.nix;
+              # home-manager.extraSpecialArgs = { inherit splitpkgs; };
             })
           ];
-          
+      };
+      splitberry = {
+        modules  = [
+          nixpkgs.nixosModules.notDetected
+          ({ pkgs, ... }: {
+              system.stateVersion = "22.05";
+              # Let 'nixos-version --json' know about the Git revision of this flake.
+              system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+              imports = [
+                ./machines/splitberry/hardware-configuration.nix
+                ./users/marijan/base.nix
+                ./environments/common.nix
+                ./options/wireless.nix
+                ./services/nastavi.nix
+              ];
+          })
+        ];
       };
     };
 
