da1bdca002ab1bfe74d356e858a4f6885f730aa3
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-07-06 14:42:57 +0200

    fixup

---
+++ get-rid-of-home-manager.nix
@@ -0,0 +1,7 @@
+files:
+let
+  link = origin: target: "L+ ${target} - - - - ${origin}";
+  home = "/home/marijan/";
+in {
+  systemd.tmpfiles.rules = map ({origin,target}: link origin "${home}${target}") files;
+}
