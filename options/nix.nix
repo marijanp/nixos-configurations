c24b3288bdcd15f3f1704eadc926e51f86c099a9
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-11 17:31:06 +0200

    nix: remove iohk cachix

--- options/nix.nix
+++ options/nix.nix
@@ -6,11 +6,9 @@
   '';
   nix.binaryCachePublicKeys = [
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
   ];
   nix.binaryCaches = [
     "https://cache.iog.io"
-    "https://iohk.cachix.org"
   ];
   nix.buildCores = 4;
   nix.trustedUsers = [ "root" "marijan" ];
