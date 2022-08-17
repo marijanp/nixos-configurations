144c0810c6294ef907a3cc6ed3c0483b57009642
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-17 19:53:03 +0200

    security: enable yubikey pam auth

--- options/gpg.nix
+++ options/gpg.nix
@@ -2,6 +2,17 @@
 {
   programs.gnupg.agent = {
     enable = true;
+    enableSSHSupport = true;
     pinentryFlavor = "curses";
+  };
+  services.pcscd.enable = true;
+  security.pam = {
+    u2f.enable = true;
+    yubico = {
+      enable = true;
+      debug = false;
+      control = "sufficient";
+      mode = "challenge-response";
+    };
   };
 }
