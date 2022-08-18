5115b8fa4aff2ad7e38d81f621ecd942d583a66d
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-18 09:54:09 +0200

    options/sound: refactor

--- options/sound.nix
+++ options/sound.nix
@@ -1,12 +1,8 @@
 { config, pkgs, ... }:
 {
-  # Enable sound.
-  sound.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-    jack.enable = true;
   };
-
 }
