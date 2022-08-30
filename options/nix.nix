fb5ccb529de63a6afff19fee93ff7c58e91a2c29
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-08-30 14:12:23 +0200

    options/nix: add ardana substituter

--- options/nix.nix
+++ options/nix.nix
@@ -11,9 +11,11 @@
       sandbox = true;
       trusted-substituters = [
         "https://cache.iog.io"
+        "https://private-ardanalabs.cachix.org"
       ];
       trusted-public-keys = [
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "private-ardanalabs.cachix.org-1:BukERsr5ezLsqNT1T7zlS7i1+5YHsuxNTdvcgaI7I6Q="
       ];
     };
   };
