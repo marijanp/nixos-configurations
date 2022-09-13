59dcefbdacad4c8fc22b7f733fd5027b9a4df815
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-09-13 18:27:22 +0200

    options/wireless: enable userControlled i.e. wpa_supplicant service

--- options/wireless.nix
+++ options/wireless.nix
@@ -1,41 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-  networking.wireless.networks = {
-    "laganini" = {
-      pskRaw = "6bca9bedd7841c7cc6269a477805c6d2493f6fcc654d451455c7600411238153";
-    };
-    "WLAN-574446" = {
-      pskRaw = "ca0f1467dbc64525d536fd4bc1dd36361868d3c72a2c51a6b043ab1520e38270";
-    };
-    "WLAN-280772" = {
-      pskRaw = "ad679f5e4fb1fcce5444843e46e3e0f8e288efeda0116d84c98c104f55fbfadf";
-    };
-    "SWR2" = {
-      pskRaw = "8f204684620bb1739b2535e91e11216a88e1075ce07a8a56d5c6c6088cd05b97";
-    };
-    "HUB" = {
-      pskRaw = "cee96167f2a4e92a0588b6055483f76ac980c005a345a886e3ceaa6f020c15ff";
-    };
-    "EspressoGuests" = {
-      pskRaw = "bf5adb74626711019a31f2e47f1babf98dcccbc4e987d3c266ace7ea9ea3ae0e";
-    };
-    "Excellence" = {
-      pskRaw = "e751221d62e9fa740710e50f5152a39288f497819f26c9af086257469b9d2829";
-    };
-    "FZI-PRIVATE-DEVICES" = {
-      auth = ''
-        key_mgmt=WPA-EAP
-        identity="petricev"
-        password=hash:4db5ccc75eb1bc29cd92e41e9f762136
-      '';
-    };
-    "NOVUS" = {
-      pskRaw = "c99c6ac36851dddf610d042fd19729cad522bb96b65540b0a8293d38bbfe3bfa";
-    };
-    "Bava Bar" = {
-      pskRaw = "41ee6cf3cd5b0a02accecf96b70d8f77e8489d4d209cdb5fc8ab6adf482ca9d5";
+  networking.wireless = {
+    enable = true; # Enables wireless support via wpa_supplicant.
+    # wpa_cli
+    # scan
+    # scan_results
+    # add_network
+    # set_network 0 ssid "laganini"
+    # set_network 0 psk "secret"
+    # or set_network 0 key_mgmt NONE
+    # enable_network 0
+    userControlled.enable = true;
+    networks = {
+      "laganini".pskRaw = "6bca9bedd7841c7cc6269a477805c6d2493f6fcc654d451455c7600411238153";
+      "WLAN-574446".pskRaw = "ca0f1467dbc64525d536fd4bc1dd36361868d3c72a2c51a6b043ab1520e38270";
+      "Bava Bar".pskRaw = "41ee6cf3cd5b0a02accecf96b70d8f77e8489d4d209cdb5fc8ab6adf482ca9d5";
+      "POBLADO PARK".pskRaw = "1f1facc940c4a4abc5c347b4abb0f319a56dcd629358fd4302be0a18aaad54b9";
     };
   };
 }
