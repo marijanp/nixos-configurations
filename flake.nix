ba30aaac98aa5cefef4266a9e79e5473f35e02eb
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-09-13 18:26:08 +0200

    flake: update nixpgks to unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "marijanp's NixOS system configurations";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager.url = github:nix-community/home-manager/release-22.05;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
