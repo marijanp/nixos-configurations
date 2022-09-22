40e9d85c3f6683066fcd3f15304e4c4b35b280a6
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-09-22 20:39:23 +0200

    remove unneeded gpg patch after nixpkgs update

--- options/gpg.nix
+++ options/gpg.nix
@@ -5,16 +5,4 @@
     enableSSHSupport = true;
     pinentryFlavor = "curses";
   };
-  # TODO: remove when https://nixpk.gs/pr-tracker.html?pr=187562 reaches nixpkgs-unstable
-  programs.gnupg.package = pkgs.gnupg.overrideAttrs (old:
-    if old.version == "2.3.7" then {
-      patches = old.patches ++ [
-        (pkgs.fetchpatch {
-          name = "fix-yubikey.patch";
-          url = "https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=patch;h=f34b9147eb3070bce80d53febaa564164cd6c977;hp=95651d1a4fec9bc5e36f623b2cdcc6a35e0c30bb";
-          sha256 = "sha256-KD8H6N9oGx+85SetHx1OJku28J2dmsj/JkJudknCueU=";
-        })
-      ];
-    } else { }
-  );
 }
