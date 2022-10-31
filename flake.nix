7bf40191872a1741df9a3db4912ac794c139e74b
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-10-31 12:14:28 -0500

    flake: update nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "marijanp's NixOS system configurations";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    home-manager.url = github:nix-community/home-manager/release-22.05;
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
