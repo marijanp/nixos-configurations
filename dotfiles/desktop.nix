6843fa05905d001b4376a3f099211e2af9a55310
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-10-31 07:39:42 -0500

    dotfiles/desktop: enable direnv

--- dotfiles/desktop.nix
+++ dotfiles/desktop.nix
@@ -56,6 +56,9 @@
     inactiveInterval = 5;
   };
 
+  # see https://github.com/nix-community/nix-direnv
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
 
   home.packages = with pkgs; [
     cachix
