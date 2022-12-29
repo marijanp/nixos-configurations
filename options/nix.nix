e54e0e442d77f4f9b225ece767c99d9fc00d71d9
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-12-29 18:13:07 -0500

    options/nix: add split binary cache

--- options/nix.nix
+++ options/nix.nix
@@ -9,10 +9,12 @@
       trusted-users = [ "root" "marijan" ];
       sandbox = true;
       trusted-substituters = [
+        "https://split.cachix.org"
         "https://cache.iog.io"
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
+        "split.cachix.org-1:kfgN2OWSb/v2WsOU8Jhf4FnDUvay7s/2J09sxPYs7ds="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
