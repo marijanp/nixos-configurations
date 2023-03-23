5cc384204ed8ce26e9315269a020ce27fc4c60d8
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-03-23 14:23:20 +0100

    options/wireless: add new network

--- options/wireless.nix
+++ options/wireless.nix
@@ -17,6 +17,7 @@
       "WLAN-574446".pskRaw = "ca0f1467dbc64525d536fd4bc1dd36361868d3c72a2c51a6b043ab1520e38270";
       "Bava Bar".pskRaw = "41ee6cf3cd5b0a02accecf96b70d8f77e8489d4d209cdb5fc8ab6adf482ca9d5";
       "Yo mamas iPhone".pskRaw = "027dc1b59db80540334b4b58d403c840ea6497296285045a1facb24b76105de2";
+      "Trafo Hub Member".pskRaw = "c64e25841b9a85ad818b9dfea5ec436826dd6efda1d95e27119c0f32449bdc5b";
       "MarriottBonvoy_Guest" = { };
       "Starbucks WiFi" = { };
       "_Free JFK WiFi" = { };
