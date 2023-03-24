5c1fb76870f47e11c694b4850b929e0fe747de26
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-03-24 11:40:35 +0100

    dotfiles/nvim: update lsp and completion config

--- dotfiles/nvim/cmp.lua
+++ dotfiles/nvim/cmp.lua
@@ -1,22 +1,26 @@
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require'cmp_nvim_lsp'.default_capabilities(capabilities)
 
-local luasnip = require 'luasnip'
 local cmp = require 'cmp'
 cmp.setup{
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
   snippet = {
+    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      luasnip.lsp_expand(args.body) -- use luasnip snippet engine
+      require('luasnip').lsp_expand(args.body)
     end,
   },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-h>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-l>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    -- `cmp.config.disable` removes the default `<C-y>` mapping.
+    ["<C-y>"] = cmp.config.disable,
     ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
@@ -24,14 +28,9 @@ cmp.setup{
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = true },
-
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -45,26 +44,21 @@ cmp.setup{
       vim_item.kind = string.format("%s", vim_item.kind)
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
-        luasnip = "[Snippet]",
       })[entry.source.name]
       return vim_item
     end,
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
   window = {
-    documentation = {
-      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-    },
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   experimental = {
     ghost_text = false,
     native_menu = false,
   },
 }
+
--- dotfiles/nvim/default.nix
+++ dotfiles/nvim/default.nix
@@ -1,5 +1,9 @@
 { pkgs, lib, agenix, ... }:
 {
+  home.packages = with pkgs; [
+    nil
+  ];
+
   programs.neovim = {
     enable = true;
     extraConfig = builtins.readFile ../vim/.vimrc + ''
@@ -8,45 +12,85 @@
       nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>
       nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>
       nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>
+      nnoremap <silent> gl <cmd>lua vim.diagnostic.open_float()<CR>
       nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>
-      nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
-      nnoremap <silent> <C-n> <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
-      nnoremap <silent> <C-p> <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
+      nnoremap <silent> <leader>ln <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
+      nnoremap <silent> <leader>lp <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
+      nnoremap <silent> <leader>la <cmd>lua vim.lsp.buf.code_action()<CR>
+      nnoremap <silent> <leader>lr <cmd>lua vim.lsp.buf.rename()<CR>
+      nnoremap <silent> <leader>ls <cmd>lua vim.lsp.buf.signature_help()<CR>
+      nnoremap <silent> <leader>lq <cmd>lua vim.diagnostic.setloclist()<CR>
+    '';
+    extraLuaConfig = ''
+      vim.diagnostic.config({
+        virtual_text = false,
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+          focusable = true,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
     '';
     plugins = with pkgs.vimPlugins; [
+      # utils
       vim-airline
-      vim-signify # show git diff
-      vim-nix
+      vim-signify # shows git diff
       {
         plugin = rainbow;
         config = "let g:rainbow_active = 1";
       }
+      {
+        plugin = indent-blankline-nvim;
+        type = "lua";
+        config = ''
+          require("indent_blankline").setup {
+            show_current_context = true,
+          }
+        '';
+      }
+      # languages
+      vim-nix
       haskell-vim
       purescript-vim
-      cmp_luasnip # required by nvim-cmp
-      luasnip # required by nvim-cmp
-      cmp-nvim-lsp # required by nvim-cmp
+      # snippets
+      luasnip
+      cmp_luasnip
+      # completion
+      cmp-nvim-lsp # lsp completions
       {
         plugin = nvim-cmp;
         type = "lua";
         config = builtins.readFile ./cmp.lua;
       }
+      # lsp
       {
         plugin = nvim-lspconfig;
         type = "lua";
         config = ''
           require'lspconfig'.hls.setup{
             cmd = { "haskell-language-server", "--lsp" }
+          , capabilities = capabilities
+          , settings = {
+              haskell = {
+                formattingProvider = "fourmolu"
+              }
+            }
           }
-          require'lspconfig'.nil_ls.setup{}
-          require'lspconfig'.purescriptls.setup{}
+          require'lspconfig'.nil_ls.setup{
+            capabilities = capabilities
+          }
+          require'lspconfig'.purescriptls.setup{
+            capabilities = capabilities
+          }
         '';
       }
     ];
   };
-
-  home.packages = with pkgs; [
-    nil
-  ];
 }
 
