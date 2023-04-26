02d0a57e5c07c71b8d83b95737734e96af40f970
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-04-26 12:22:07 +0200

    options/wireless: update

--- options/wireless.nix
+++ options/wireless.nix
@@ -13,11 +13,14 @@
     # enable_network 0
     userControlled.enable = true;
     networks = {
-      "laganini".pskRaw = "6bca9bedd7841c7cc6269a477805c6d2493f6fcc654d451455c7600411238153";
+      "lambda".pskRaw = "0d0d8d97b38341789cd449e0ab875d75fa78a53d1648037229859f4e1cfe24e9";
       "WLAN-574446".pskRaw = "ca0f1467dbc64525d536fd4bc1dd36361868d3c72a2c51a6b043ab1520e38270";
       "Bava Bar".pskRaw = "41ee6cf3cd5b0a02accecf96b70d8f77e8489d4d209cdb5fc8ab6adf482ca9d5";
       "Yo mamas iPhone".pskRaw = "027dc1b59db80540334b4b58d403c840ea6497296285045a1facb24b76105de2";
       "Trafo Hub Member".pskRaw = "c64e25841b9a85ad818b9dfea5ec436826dd6efda1d95e27119c0f32449bdc5b";
+      "jongenet".pskRaw = "15f1efca85bd2f654d649cea6f568ded74cb70cc2700c30a49827b45ec9328e3";
+      "FRITZ!Box 7530 RR".pskRaw = "8a6f022cf4b922d938f4ea579a53f6512809897ced4f2585bf42f3c02112a293";
+      "scaleUp_guests".pskRaw = "5cfd8d6e48fac65c744ab7baf6764fa03d8f6f885b8bbf8e0a137af98a4e3c4f";
       "MarriottBonvoy_Guest" = { };
       "Starbucks WiFi" = { };
       "_Free JFK WiFi" = { };
