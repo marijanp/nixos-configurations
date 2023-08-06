41d3895433cc1b9f2ead020ecb3006bf8b3ba980
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-08-06 19:12:36 +0200

    networking: enable nat for nixos-containers to access internet

--- options/networking.nix
+++ options/networking.nix
@@ -1,10 +1,13 @@
-{ pkgs, config, hostName, ... }: {
-
-
+{ pkgs, config, hostName, ... }:
+{
   networking = {
     inherit hostName;
     enableIPv6 = false;
     firewall.allowedTCPPorts = config.services.openssh.ports;
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
   };
+  networking.firewall.trustedInterfaces = [ "ve-+" ];
+  networking.nat.enable = true;
+  networking.nat.internalInterfaces = [ "ve-+" ];
+  networking.nat.externalInterface = "wlp1s0";
 }
