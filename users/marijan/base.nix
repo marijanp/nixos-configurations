7e52364d973a7ba283655151897abc780e931dd3
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-09-04 18:07:13 -0400

    users: make users immutable

--- users/marijan/base.nix
+++ users/marijan/base.nix
@@ -1,15 +1,19 @@
 { config, pkgs, lib, ... }:
 {
-  users.users.marijan = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = [ "wheel" ];
+  users = {
+    mutableUsers = false;
+    users.marijan = {
+      isNormalUser = true;
+      uid = 1000;
+      hashedPassword = "$y$j9T$p9f//hAS.s8uGIQ6OpnKt1$o8iWFumDlAgqm8b4f2D9rb7w05yK24HNkWZf71TbnH3";
+      extraGroups = [ "wheel" ];
 
-    openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKJMTVrY0qbJfu2g1TocLxRrYc/AjlnUuR35Y4biaLThAAAABHNzaDo="
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuIz0wFEPdBpR8RZkR2dnX57TPlsv69sUN0I9WjR6jj marijan@laganini"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEm0jqNuG+NtkVVqa8s+kB+klSYCEctWbrskSiT440sW marijan@split"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGRDEu0YvbI33NNrAPrFKhl2EHs/1cNfSv6g7rLrvD19 marijan@iphone"
-    ];
+      openssh.authorizedKeys.keys = [
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKJMTVrY0qbJfu2g1TocLxRrYc/AjlnUuR35Y4biaLThAAAABHNzaDo="
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuIz0wFEPdBpR8RZkR2dnX57TPlsv69sUN0I9WjR6jj marijan@laganini"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEm0jqNuG+NtkVVqa8s+kB+klSYCEctWbrskSiT440sW marijan@split"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGRDEu0YvbI33NNrAPrFKhl2EHs/1cNfSv6g7rLrvD19 marijan@iphone"
+      ];
+    };
   };
 }
