ab4123680b99af44681c5675604dc97c97480219
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-09-08 12:06:49 +0200

    remove archive

--- archive/get-rid-of-home-manager.nix
+++
@@ -1,8 +0,0 @@
-files:
-let
-  link = origin: target: "L+ ${target} - - - - ${origin}";
-  home = "/home/marijan/";
-in
-{
-  systemd.tmpfiles.rules = map ({ origin, target }: link origin "${home}${target}") files;
-}
