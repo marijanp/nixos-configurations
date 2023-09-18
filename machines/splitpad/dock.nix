d4cb526c5f7d829644a35dccf611f01a3bc9c66b
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-09-18 13:23:59 +0200

    machines/split: add switch for projector

--- machines/splitpad/dock.nix
+++ machines/splitpad/dock.nix
@@ -1,5 +1,10 @@
 { pkgs, config, ... }: {
   programs.bash.shellAliases = {
+    switch-projector = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${builtins.toString config.services.xserver.dpi} \
+        --output eDP-1 --auto \
+        --output DP-1 --mode 1920x1080 --scale 2x2 --pos 2880x0
+    '';
     switch-dock = ''
       ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${builtins.toString config.services.xserver.dpi} \
         --output eDP-1 --off \
