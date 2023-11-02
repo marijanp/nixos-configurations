fabf56030bb05355907bb73de3fee2c025763b25
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-11-02 19:06:07 +0100

    secrets: rekey with new machine key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 let
   marijan.keys = [ "age1yubikey1q0tpa48d03dy59jcsjsx5a8zv0p8msr89ut7xgr64x5ujgkrn0ceulx4zwv" ];
-  localhost.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMJrYYVComJgquUVzDD91Z3roMKZQdYIGyZqbMjuOQfjKA" ];
+  split.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMY3yyshqtmN2qO2RlifbxiuKfNg1jsn+RbA2+/bgwgc";
+  splitpad.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMJrYYVComJgquUVzDD91Z3roMKZQdYIGyZqbMjuOQfjKA";
+  system.keys = [ splitpad.key split.key ];
 in
 {
-  "rclone-drive-config.age".publicKeys = marijan.keys ++ localhost.keys;
-  "smos-google-calendar-source.age".publicKeys = marijan.keys ++ localhost.keys;
+  "rclone-drive-config.age".publicKeys = marijan.keys ++ system.keys;
+  "smos-google-calendar-source.age".publicKeys = marijan.keys ++ system.keys;
 }
