c70a4b182c452fcf203b828d44e1b2af1c0dabce
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-01-10 09:24:13 +0100

    nixpkgs: permit insecure electron version temporarily

--- options/nixpkgs.nix
+++ options/nixpkgs.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }:
 {
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages =
+    lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
   nixpkgs.overlays = [
   ];
 }
