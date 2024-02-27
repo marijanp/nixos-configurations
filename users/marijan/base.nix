9e48296c80a9e1b4a8b2f211bacbcc89d617b99a
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-02-27 16:29:55 +0700

    users/marijan: remove old ssh keys

--- users/marijan/base.nix
+++ users/marijan/base.nix
@@ -10,8 +10,6 @@
 
       openssh.authorizedKeys.keys = [
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKJMTVrY0qbJfu2g1TocLxRrYc/AjlnUuR35Y4biaLThAAAABHNzaDo="
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuIz0wFEPdBpR8RZkR2dnX57TPlsv69sUN0I9WjR6jj marijan@laganini"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEm0jqNuG+NtkVVqa8s+kB+klSYCEctWbrskSiT440sW marijan@split"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGRDEu0YvbI33NNrAPrFKhl2EHs/1cNfSv6g7rLrvD19 marijan@iphone"
       ];
     };
