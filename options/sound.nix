7fb0bb4d44717f991cea3fbd38def3753daa0b98
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-02-27 17:02:35 +0700

    options/sound: remove not working airplay conf

--- options/sound.nix
+++ options/sound.nix
@@ -7,15 +7,4 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-  environment.etc."pipewire/pipewire.conf.d/airplay.conf" = {
-    mode = "0444";
-    text = ''
-      context.modules = [
-        {
-          name = libpipewire-module-raop-discover
-          args = {}
-        }
-      ]
-    '';
-  };
 }
