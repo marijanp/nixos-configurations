54d16f2de2c8465c86d4c36a45b8a236ac0ffa9a
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-02-27 17:03:17 +0700

    options/nixpkgs: remove permittedInsecurePackages

--- options/nixpkgs.nix
+++ options/nixpkgs.nix
@@ -3,8 +3,6 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
-      permittedInsecurePackages =
-        lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
     };
     overlays = [
     ];
