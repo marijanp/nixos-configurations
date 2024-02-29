d78643269816aae603e303dab70e1be3f24d3375
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-02-29 13:18:55 +0700

    options/networking: remove nat

--- options/networking.nix
+++ options/networking.nix
@@ -3,11 +3,6 @@
   networking = {
     inherit hostName;
     enableIPv6 = false;
-    firewall.allowedTCPPorts = config.services.openssh.ports;
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
   };
-  networking.firewall.trustedInterfaces = [ "ve-+" ];
-  networking.nat.enable = true;
-  networking.nat.internalInterfaces = [ "ve-+" ];
-  networking.nat.externalInterface = "wlp1s0";
 }
