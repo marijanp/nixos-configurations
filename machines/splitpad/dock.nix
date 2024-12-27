9ce546ef573985f1052167519b6fb9cdd293805a
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-12-27 11:26:40 +0100

    machines/splitpad: refactor switch-to/from-dock to be parametric

--- machines/splitpad/dock.nix
+++ machines/splitpad/dock.nix
@@ -1,24 +1,34 @@
 { pkgs, config, ... }: {
-  programs.bash.shellAliases = {
-    switch-to-dock = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${builtins.toString config.services.xserver.dpi} \
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "switch-to-dock" ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${
+        builtins.toString config.services.xserver.dpi
+      } \
         --output eDP-1 --off \
-        --output DP-9 --mode 1920x1080 --scale 2x2 \
-        --output DP-10 --mode 1920x1080 --scale 2x2 --pos 3840x0
-    '';
-    switch-from-dock = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${builtins.toString config.services.xserver.dpi} \
+        --output DP-"$1" --mode 1920x1080 --scale 2x2 \
+        --output DP-"$2" --mode 1920x1080 --scale 2x2 --pos 3840x0
+    '')
+    (pkgs.writeShellScriptBin "switch-from-dock" ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${
+        builtins.toString config.services.xserver.dpi
+      } \
         --output eDP-1  --auto \
-        --output DP-9 --off \
-        --output DP-10 --off
-    '';
+        --output "DP-$1" --off \
+        --output "DP-$2" --off
+    '')
+  ];
+  programs.bash.shellAliases = {
     switch-to-projector = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${builtins.toString config.services.xserver.dpi} \
+      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${
+        builtins.toString config.services.xserver.dpi
+      } \
         --output eDP-1 --auto \
         --output DP-1 --mode 1920x1080 --scale 2x2 --pos 2880x0
     '';
     switch-from-projector = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${builtins.toString config.services.xserver.dpi} \
+      ${pkgs.xorg.xrandr}/bin/xrandr --dpi ${
+        builtins.toString config.services.xserver.dpi
+      } \
         --output eDP-1 --auto \
         --output DP-1 --off
     '';
