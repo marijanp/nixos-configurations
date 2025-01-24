3158744ec6961a0c81ff1cbb4f7960e63e8c0e92
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-01-24 16:05:37 +0100

    environments/work: add virt-manager

--- environments/work.nix
+++ environments/work.nix
@@ -1,3 +1,7 @@
 {
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["marijan"];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
 }
 
