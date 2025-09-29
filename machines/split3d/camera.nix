73652508b1d3df4b275ead9583aed96e40be5a85
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-09-29 19:36:23 +0200

    machines/split3d: lower camera fps

--- machines/split3d/camera.nix
+++ machines/split3d/camera.nix
@@ -4,7 +4,7 @@
   services.ustreamer = {
     enable = true;
     device = "/dev/video0";
-    extraArgs = [ "--resolution=1280x960" "--quality=100" ];
+    extraArgs = [ "--resolution=1280x960" "--quality=100" "--desired-fps=10" ];
   };
   boot.kernelModules = [ "bcm2835-v4l2" ];
 }
