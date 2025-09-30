9dc273bddc6ee221bc001bd58a83eb0d9bc3ead9
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-09-30 18:33:31 +0200

    camera: add comment

--- machines/split3d/camera.nix
+++ machines/split3d/camera.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 {
+  # Manually add this to the /boot/config.txt
+  # gpu_mem=128
+  # start_x=1
+  boot.kernelModules = [ "bcm2835-v4l2" ];
   environment.systemPackages = with pkgs; [ v4l-utils ];
   services.ustreamer = {
     enable = true;
     device = "/dev/video0";
     extraArgs = [ "--resolution=1280x960" "--quality=100" "--desired-fps=10" ];
   };
-  boot.kernelModules = [ "bcm2835-v4l2" ];
 }
