bdde55ecef12f982549959943cc0afee7a28c2a9
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-12-07 11:49:10 +0700

    dotfiles/nvim: change theme

--- dotfiles/nvim/default.nix
+++ dotfiles/nvim/default.nix
@@ -72,8 +72,14 @@
         plugin = nightfox-nvim;
         type = "lua";
         config = /* lua */ ''
-          require('nightfox')
-          vim.cmd.colorscheme("nordfox")
+          require('nightfox').setup({
+            palettes = {
+              carbonfox = {
+                bg1 = "#000000", -- Black background
+              }
+            },
+          })
+          vim.cmd.colorscheme("carbonfox")
         '';
       }
       # utils
