4028131ac727ea277fa0a918abe966f186a1fa73
Marijan Petričević <marijan.petricevic94@gmail.com>
2026-01-10 11:43:32 +0100

    machines/splitberry/networking: use static ip address

--- machines/splitberry/networking.nix
+++ machines/splitberry/networking.nix
@@ -1,11 +1,31 @@
 { config, pkgs, lib, ... }:
 {
-  networking.networkmanager.enable = true;
+  #networking = {
+  #  wireless = {
+  #    interfaces = [ "wlan0" ];
+  #  };
+  #};
   networking = {
-    wireless = {
-      interfaces = [ "wlan0" ];
+    defaultGateway = "192.168.1.1";
+    firewall = {
+      allowedTCPPorts = [
+        3000
+        53 # DNS
+        853 # DNS over TLS
+      ];
+      allowedUDPPorts = [
+        53 # DNS
+        5353 # DNS over QUIC
+      ];
+    };
+    interfaces.eth0 = {
+      useDHCP = lib.mkDefault false;
+      ipv4.addresses = [
+        {
+          address = "192.168.1.200";
+          prefixLength = 24;
+        }
+      ];
     };
   };
-  networking.interfaces.eth0.useDHCP = lib.mkDefault true;
-  networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
 }
