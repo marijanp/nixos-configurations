7360ada092824d3445433e026f18bb789b8f0284
Marijan Petričević <marijan.petricevic94@gmail.com>
2026-02-08 13:06:29 +0100

    options/nix: add cache.nixos.org trusted public key

--- options/nix.nix
+++ options/nix.nix
@@ -20,6 +20,7 @@
       ];
       trusted-public-keys = [
         "split.cachix.org-1:kfgN2OWSb/v2WsOU8Jhf4FnDUvay7s/2J09sxPYs7ds="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
 
