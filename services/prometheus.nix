72fc1f5e5cb272e657799ddc4f3ef9f7184edb47
Marijan Petričević <marijan.petricevic94@gmail.com>
2026-02-19 21:25:12 +0100

    prometheus: add gpu exporter

--- services/prometheus.nix
+++ services/prometheus.nix
@@ -1,11 +1,18 @@
 { config, ... }:
 {
-  services.prometheus.exporters.node = {
-    enable = true;
-    enabledCollectors = [ "systemd" ];
-    port = 9100;
+  services.prometheus.exporters = {
+    node = {
+      enable = true;
+      enabledCollectors = [ "systemd" ];
+      port = 9100;
+    };
+    nvidia-gpu = {
+      enable = config.networking.hostName == "split";
+      port = 9102;
+    };
   };
   networking.firewall.interfaces."wg0".allowedTCPPorts = [
     config.services.prometheus.exporters.node.port
+    config.services.prometheus.exporters.nvidia-gpu.port
   ];
 }
