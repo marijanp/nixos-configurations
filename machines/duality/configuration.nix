de2887176e13fd0f64c8ffb180867597b30af6f2
Marijan Petričević <marijan.petricevic94@gmail.com>
2026-06-24 19:36:55 -0600

    machines/duality: remove udev rules that are now in nixos-hardware

--- machines/duality/configuration.nix
+++ machines/duality/configuration.nix
@@ -96,11 +96,4 @@
     disableWhileTyping = true;
     tapping = false;
   };
-
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="sound", ATTR{id}=="amdsoundwire", RUN+="${lib.getBin pkgs.alsa-utils}/bin/amixer -c $attr{device/number} set 'tas2783-1 Amp' on"
-    ACTION=="add", SUBSYSTEM=="sound", ATTR{id}=="amdsoundwire", RUN+="${lib.getBin pkgs.alsa-utils}/bin/amixer -c $attr{device/number} set 'tas2783-2 Amp' on"
-    ACTION=="add", SUBSYSTEM=="sound", ATTR{id}=="amdsoundwire", RUN+="${lib.getBin pkgs.alsa-utils}/bin/amixer -c $attr{device/number} set 'tas2783-1 Speaker' on"
-    ACTION=="add", SUBSYSTEM=="sound", ATTR{id}=="amdsoundwire", RUN+="${lib.getBin pkgs.alsa-utils}/bin/amixer -c $attr{device/number} set 'tas2783-2 Speaker' on"
-  '';
 }
